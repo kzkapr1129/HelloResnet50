9ad51cf22592c2ccdeac1675c71ec3b58c452e08
nakayama <nakayama@nakayamanoMacBook-puro.local>
2019-05-27 15:07:40 +0900

    チェックポイントの保存

--- Train.py
+++ Train.py
@@ -3,15 +3,21 @@ from Dataset import loadTest
 from Dataset import generator
 from Dataset import NUM_CLASSES
 import math
+import os
 import keras
 import matplotlib.pyplot as plt
 
+# constant values
 BATCH_SIZE  = 3
 EPOCHS = 5
+CHECKPOINT_DIR = "train_checkpoints"
+SAVE_CHECKPOINT_ONCE_IN_N_EPOCHS = 1
 
+# load train, test data
 x_train , y_train = loadTrain()
 x_test, y_test = loadTest()
 
+# create generator
 train_gen = generator(x_train, y_train, BATCH_SIZE)
 test_gen = generator(x_test, y_test, x_test.shape[0])
 
@@ -20,10 +26,19 @@ steps_per_epoch = math.ceil(N / BATCH_SIZE)
 print("INFO: Total datas = {}".format(N))
 print("INFO: Step per epoch = {}".format(steps_per_epoch))
 
+# init checkpoint
+os.mkdir(CHECKPOINT_DIR)
+checkpoint_path = CHECKPOINT_DIR + "/" + "cp-{epoch:06d}.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+cp_callback = keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1,
+                                                 period=SAVE_CHECKPOINT_ONCE_IN_N_EPOCHS) # save checkpoint Once in 1 epoch times
+
 # start trainning
 model = keras.applications.resnet50.ResNet50(weights=None, classes=NUM_CLASSES)
 model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-history = model.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, validation_data=test_gen, validation_steps=1, epochs=EPOCHS)
+history = model.fit_generator(train_gen, steps_per_epoch=steps_per_epoch, validation_data=test_gen, validation_steps=1, callbacks=[cp_callback], epochs=EPOCHS)
 
 # save model & weights
 json_string = model.to_json()
